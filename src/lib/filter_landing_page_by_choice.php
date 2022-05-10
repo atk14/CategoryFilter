f753669ebfde20c5459c24396e7140006bc1334d
Matyas Novak <logik@centrum.cz>
2022-05-10 18:28:42 +0200

    FilterLandingPageByChoice - special_label_function option

--- src/lib/filter_landing_page_by_choice.php
+++ src/lib/filter_landing_page_by_choice.php
@@ -3,11 +3,20 @@
 class FilterLandingPageByChoice {
 
 	function __construct($options = []) {
-		$this->options = $options + [
+		$options += [
 			'label_string' => ' %s',
-			'label_function' => null,
-			'label_lowercase' => false
+			'label_lowercase' => false,
+			'label_function' => null,   //Function, that accepts choice key and returns the label for given choice. Possibly, it can accept
+																  //additional argument, passed to label. If it is not given, the label is created
+																  //according to the 'label_string' and 'label_lowercase' options
+			'special_label_function' => null, //If defined, the label with non-null argument is built by this function.
 		];
+		if($options['label_function'] && !$options['special_label_function'] &&
+		   (new ReflectionFunction($options['label_function']))->getNumberOfParameters() > 1
+		 ) {
+			$options['special_label_function'] = $options['label_function'];
+		}
+		$this->options=$options;
 	}
 
 	function setSection($section) {
@@ -22,14 +31,25 @@ class FilterLandingPageByChoice {
 		return true;
 	}
 
-	function label($arg=null) {
-		return $this->_label(current($this->section->getValues()), $null);
+	function value() {
+		return current($this->section->getValues());
 	}
 
-	function _label($id, $arg=null) {
+	function label($for=null) {
+		return $this->_label($this->value(), $for);
+	}
+
+	function _label($id, $for=null) {
+		if($for!==null) {
+			if(!$this->options['special_label_function']) {
+				return null;
+			}
+			$slf = $this->options['special_label_function'];
+			return $slf($id, $for);
+		}
 		if($this->options['label_function']) {
 			$fce=$this->options['label_function'];
-			return $fce($id, $arg);
+			return $fce($id);
 		} else {
 			$label = $this->section->getChoiceLabels()[$id];
 			if($this->options['label_lowercase']) {
