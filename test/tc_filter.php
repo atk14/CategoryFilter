48d1d1bd9e65a127739d11d4ff3849125822166b
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-12-06 09:53:33 +0100

    Testing

--- test/tc_filter.php
+++ test/tc_filter.php
@@ -30,4 +30,19 @@ class TcFilter extends TcBase {
 			$this->assertFalse(spl_object_hash($filter->sections[$section->name]) == spl_object_hash($section));
 		}
 	}
+
+	function test_usage(){
+		$options = [];
+		$filter = new Filter("cards", $options);
+		$filter->addCondition("cards.visible AND NOT cards.deleted");
+		$productJoin = $filter->addJoin("products")->where("products.card_id = cards.id AND NOT products.deleted AND products.visible")->setJoinBy("left join");
+
+		$this->assertEquals("Card", (string)$filter->getModel());
+
+		//
+
+		$options = ["model" => "Product"];
+		$filter = new Filter("cards", $options);
+		$this->assertEquals("Product", (string)$filter->getModel());
+	}
 }
